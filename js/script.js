f5b3bb01f26d313805d6dca3f90fab471714938b
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-26 23:34:20 +0500

    portfoleo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// menu show
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId)
+    nav = document.getElementById(navId)
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// active add remove menu
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    //Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
