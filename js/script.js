7545bc4f59bc8f10dd8bfaa2535ab20b0bc411ef
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-26 23:57:54 +0500

    portfoleo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // menu show
 const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId)
+    const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
     if(toggle && nav){
         toggle.addEventListener('click',()=>{
